74f78e9ef2fcb329986eb4e9505a303cd1ee504e
Jakob Schwendinger <jschwendinger.mmt-b2024@fh-salzburg.ac.at>
2026-04-13 11:26:14 +0200

    Add Upsert functions

--- worker/db/db.go
+++ worker/db/db.go
@@ -3,9 +3,11 @@ package db
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"gitlab.ct.fh-salzburg.ac.at/fhs52920/tradevise/app/worker/config"
+	"gitlab.ct.fh-salzburg.ac.at/fhs52920/tradevise/app/worker/model"
 )
 
 
@@ -17,4 +19,51 @@ func New(cfg *config.Config) *pgxpool.Pool {
 	}
 
     return pool
+}
+
+func UpsertIntraday(pool *pgxpool.Pool, symbol string, points []model.PricePoint) error {
+    for _, p := range points {
+        _, err := pool.Exec(context.Background(),
+            `INSERT INTO prices_intraday (symbol, time, price)
+             VALUES ($1, $2, $3)
+             ON CONFLICT (symbol, time) DO NOTHING`,
+            symbol,
+            time.Unix(p.Time, 0).UTC(),
+            p.Price,
+        )
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+}
+
+func UpsertWeekly(pool *pgxpool.Pool, symbol string, points []model.PricePoint) error {
+    for _, p := range points {
+        _, err := pool.Exec(context.Background(),
+            `INSERT INTO prices_weekly (symbol, date, price)
+             VALUES ($1, $2, $3)
+             ON CONFLICT (symbol, date) DO NOTHING`,
+            symbol,
+            time.Unix(p.Time, 0).UTC(),
+            p.Price,
+        )
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+}
+
+func SetSymbolDone(pool *pgxpool.Pool, symbol, currency, name string) error {
+    _, err := pool.Exec(context.Background(),
+        `UPDATE tracked_symbols
+         SET bootstrap_status = 'DONE',
+             currency = $2,
+             name = $3,
+             bootstrapped_at = NOW()
+         WHERE symbol = $1`,
+        symbol, currency, name,
+    )
+    return err
 }

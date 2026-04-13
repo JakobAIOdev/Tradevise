f4c7a1977f2d195716aa3fc64309b850fde1746f
Jakob Schwendinger <jschwendinger.mmt-b2024@fh-salzburg.ac.at>
2026-04-13 10:54:01 +0200

    Fix env load

--- worker/config/config.go
+++ worker/config/config.go
@@ -1,6 +1,11 @@
 package config
 
-import "os"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 type Config struct {
     RedisURL    string
@@ -8,9 +13,13 @@ type Config struct {
 }
 
 func Load() *Config {
+    if err := godotenv.Load(); err != nil {
+        log.Println("config: no .env file found")
+    }
+
     return &Config{
         RedisURL:    getEnv("REDIS_URL", "redis://localhost:6379"),
-        DatabaseURL: getEnv("DATABASE_URL", "postgres://tradevise:secret@localhost:5432/tradevise"),
+        DatabaseURL: getEnv("DATABASE_URL", "postgres://tradevise:secret@localhost:5432/tradevise?sslmode=disable"),
     }
 }
 
--- worker/go.mod
+++ worker/go.mod
@@ -18,3 +18,5 @@ require (
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
 )
+
+require github.com/joho/godotenv v1.5.1

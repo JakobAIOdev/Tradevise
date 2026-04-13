760287e488bec3f1f1ddb0972c6b01a3a2a35882
Jakob Schwendinger <jschwendinger.mmt-b2024@fh-salzburg.ac.at>
2026-04-13 10:30:09 +0200

    implement currency fetch for EUR

--- worker/main.go
+++ worker/main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"log"
 
+	"github.com/robfig/cron/v3"
+	converter "gitlab.ct.fh-salzburg.ac.at/fhs52920/tradevise/app/worker/currency"
 	"gitlab.ct.fh-salzburg.ac.at/fhs52920/tradevise/app/worker/scraper"
 )
 
 func main() {
+	conv := converter.New()
+
+	// daily chronjob refetch at 03:00 UTC 
+	c := cron.New()
+	c.AddFunc("0 3 * * *", func() {
+		conv.Reload()
+	})
+	c.Start()
+
+	
     price, ts, err := scraper.FetchLivePrice("AAPL")
     if err != nil {
         log.Fatal(err)
     }
-    log.Printf("AAPL: $%.2f at %d", price, ts)
+
+	priceEUR := conv.ToEUR(price, "USD")
+    log.Printf("AAPL: %.2f€ at %d", priceEUR, ts)
 }

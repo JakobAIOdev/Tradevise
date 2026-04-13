5c00a26585673b253e62ea07d1dfa41d338bbf4f
Jakob Schwendinger <jschwendinger.mmt-b2024@fh-salzburg.ac.at>
2026-04-13 13:01:02 +0200

    Add routes

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,8 +1,32 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import PortfolioPage from './pages/PortfolioPage'
+import DiscoverPage from './pages/DiscoverPage'
+import StockDetailPage from './pages/StockDetailPage'
+import RankedPage from './pages/RankedPage'
+import GroupPage from './pages/GroupPage'
+import PublicLayout from './layouts/PublicLayout'
+import DashboardLayout from './layouts/DashboardLayout'
+
 function App() {
   return (
-    <>
-      <h1>Tradevise</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+
+        <Route element={<DashboardLayout />}>
+          <Route path="/portfolio" element={<PortfolioPage />} />
+          <Route path="/search" element={<DiscoverPage />} />
+          <Route path="/stock/:ticker" element={<StockDetailPage />} />
+          <Route path="/ranked" element={<RankedPage />} />
+          <Route path="/groups" element={<GroupPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

2d79d605c5130bd1fff4e02f80750c076b34310c
Jakob Schwendinger <jschwendinger.mmt-b2024@fh-salzburg.ac.at>
2026-04-14 12:29:01 +0200

    Add searchbar to discover page

--- app/frontend/src/pages/DiscoverPage.tsx
+++ app/frontend/src/pages/DiscoverPage.tsx
@@ -1,4 +1,5 @@
 import PageTitle from '../components/PageTitle'
+import { SearchBar } from '../components/discover/SearchBar'
 import StockGrid from '../components/discover/StockGrid'
 import { MOCK_CRYPTOS, MOCK_STOCKS } from '../mock/stocks'
 
@@ -6,6 +7,9 @@ export default function DiscoverPage() {
   return (
     <div>
       <PageTitle title="Discover" />
+      <div>
+        <SearchBar className="mb-60 w-full max-w-140" />
+      </div>
       <div className="flex flex-col gap-60">
         <StockGrid title="Popular Stocks" stocks={MOCK_STOCKS} />
         <StockGrid title="Popular Cryptos" stocks={MOCK_CRYPTOS} />

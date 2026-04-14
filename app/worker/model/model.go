518fe8678f4037c3b5de3fe13471b0dd19fd2e91
Jakob Schwendinger <jschwendinger.mmt-b2024@fh-salzburg.ac.at>
2026-04-14 18:36:41 +0200

    Add new fields to model

--- app/worker/model/model.go
+++ app/worker/model/model.go
@@ -3,57 +3,60 @@ package model
 import "time"
 
 type PriceIntraday struct {
-    Symbol string
-    Time   time.Time
-    Price  float64
+	Symbol string
+	Time   time.Time
+	Price  float64
 }
 
 type PriceWeekly struct {
-    Symbol string
-    Date   time.Time
-    Price  float64
+	Symbol string
+	Date   time.Time
+	Price  float64
 }
 
 type TrackedSymbol struct {
-    Symbol          string
-    Name            string
-    Currency        string
-    BootstrapStatus string // PENDING | BOOTSTRAPPING | DONE | FAILED
-    BootstrappedAt  *time.Time
+	Symbol          string
+	Name            string
+	Currency        string
+	BootstrapStatus string // PENDING | BOOTSTRAPPING | DONE | FAILED
+	BootstrappedAt  *time.Time
 }
 
 // Redis Payload
 type LivePriceEvent struct {
-    Symbol        string  `json:"symbol"`
-    Price         float64 `json:"price"`
-    Time          int64   `json:"time"`
-    BootstrapDone bool    `json:"bootstrapDone,omitempty"`
+	Symbol        string  `json:"symbol"`
+	Price         float64 `json:"price"`
+	Time          int64   `json:"time"`
+	PreviousClose float64 `json:"previousClose,omitempty"`
+	Change        float64 `json:"change,omitempty"`
+	ChangePercent float64 `json:"changePercent,omitempty"`
+	BootstrapDone bool    `json:"bootstrapDone,omitempty"`
 }
 
 type StockMeta struct {
-    Symbol           string  `json:"symbol"`
-    Name             string  `json:"name"`
-    Currency         string  `json:"currency"`
-    Exchange         string  `json:"exchange"`
-    PreviousClose    float64 `json:"previousClose"`
-    DayHigh          float64 `json:"dayHigh"`
-    DayLow           float64 `json:"dayLow"`
-    FiftyTwoWeekHigh float64 `json:"fiftyTwoWeekHigh"`
-    FiftyTwoWeekLow  float64 `json:"fiftyTwoWeekLow"`
-    Volume           int     `json:"volume"`
+	Symbol           string  `json:"symbol"`
+	Name             string  `json:"name"`
+	Currency         string  `json:"currency"`
+	Exchange         string  `json:"exchange"`
+	PreviousClose    float64 `json:"previousClose"`
+	DayHigh          float64 `json:"dayHigh"`
+	DayLow           float64 `json:"dayLow"`
+	FiftyTwoWeekHigh float64 `json:"fiftyTwoWeekHigh"`
+	FiftyTwoWeekLow  float64 `json:"fiftyTwoWeekLow"`
+	Volume           int     `json:"volume"`
 }
 
 // Scraper intern
 type PricePoint struct {
-    Time  int64
-    Price float64
+	Time  int64
+	Price float64
 }
 
 type FetchResult struct {
-    Symbol string
-    Meta   struct {
-        RegularMarketPrice float64
-        Currency           string
-    }
-    Points []PricePoint
+	Symbol string
+	Meta   struct {
+		RegularMarketPrice float64
+		Currency           string
+	}
+	Points []PricePoint
 }

7dedf3de2fcde2c9601d373f0f484a29bfb3479b
Jakob Schwendinger <jschwendinger.mmt-b2024@fh-salzburg.ac.at>
2026-04-15 17:40:30 +0200

    Add modalRoot

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -11,6 +11,8 @@ import DashboardLayout from './layouts/DashboardLayout'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthError } from './lib/api'
 import { useAuthBootstrap } from './hooks/useAuthBootstrap'
+import DevLoginPage from './pages/DevLoginPage'
+import ModalRoot from './components/modal/ModalRoot'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -29,22 +31,26 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       {isAuthInitialized && (
-        <BrowserRouter>
-          <Routes>
-            <Route element={<PublicLayout />}>
-              <Route path="/" element={<LandingPage />} />
-            </Route>
+        <>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<PublicLayout />}>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<DevLoginPage />} />
+              </Route>
 
-            <Route element={<DashboardLayout />}>
-              <Route path="/home" element={<HomePage />} />
-              <Route path="/portfolio" element={<PortfolioPage />} />
-              <Route path="/discover" element={<DiscoverPage />} />
-              <Route path="/detail/:ticker" element={<StockDetailPage />} />
-              <Route path="/ranking" element={<RankedPage />} />
-              <Route path="/groups" element={<GroupPage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+              <Route element={<DashboardLayout />}>
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/portfolio" element={<PortfolioPage />} />
+                <Route path="/discover" element={<DiscoverPage />} />
+                <Route path="/detail/:ticker" element={<StockDetailPage />} />
+                <Route path="/ranking" element={<RankedPage />} />
+                <Route path="/groups" element={<GroupPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+          <ModalRoot />
+        </>
       )}
     </QueryClientProvider>
   )
